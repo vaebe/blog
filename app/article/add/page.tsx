389d5e0c53b594de99f3724712b799285ca73b00
vaebe <18137693952@163.com>
2024-08-22 21:09:10 +0800

    feat(editor): 集成 novel 富文本编辑器并添加默认值支持

--- app/article/add/page.tsx
+++ app/article/add/page.tsx
@@ -1,5 +1,16 @@
+"use client"
+
+import Editor from "@/components/editor/advanced-editor";
+import { JSONContent } from "novel";
+import { useState } from "react";
+import { defaultValue } from "./default-value";
+
 export default function Component() {
+  const [value, setValue] = useState<JSONContent>(defaultValue);
+  
   return (
-    <div>添加文章</div>
+    <div>
+       <Editor initialValue={value} onChange={setValue} />
+    </div>
   )
 }
