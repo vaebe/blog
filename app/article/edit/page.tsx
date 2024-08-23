2656b103773857aac8d7dfe53dd5a7fadb4ab780
vae <18137693952@163.com>
2024-08-23 10:11:34 +0800

    feat: 增加文章查看页面

--- app/article/edit/page.tsx
+++ app/article/edit/page.tsx
@@ -3,33 +3,10 @@
 import { useState } from 'react'
 
 import './style.css'
+import bytemdPlugins from '@/lib/bytemdPlugins'
 
 import { Editor } from '@bytemd/react'
 import zh_Hans from 'bytemd/locales/zh_Hans.json';
-import breaks from '@bytemd/plugin-breaks';
-import frontmatter from '@bytemd/plugin-frontmatter';
-import gemoji from '@bytemd/plugin-gemoji';
-import gfm from '@bytemd/plugin-gfm';
-import highlightSsr from '@bytemd/plugin-highlight-ssr';
-import mediumZoom from '@bytemd/plugin-medium-zoom';
-import mermaid from '@bytemd/plugin-mermaid';
-import footnotes from '@bytemd/plugin-footnotes';
-import math from '@bytemd/plugin-math-ssr';
-import mermaid_zhHans from '@bytemd/plugin-mermaid/lib/locales/zh_Hans.json';
-import math_zhHans from '@bytemd/plugin-math/lib/locales/zh_Hans.json';
-import gfm_zhHans from '@bytemd/plugin-gfm/lib/locales/zh_Hans.json';
-
-const plugins = [
-  breaks(),
-  frontmatter(),
-  gemoji(),
-  gfm({ locale: gfm_zhHans }),
-  math({ locale: math_zhHans }),
-  highlightSsr(),
-  mermaid({ locale: mermaid_zhHans }),
-  mediumZoom(),
-  footnotes()
-];
 
 export default function Component() {
 
@@ -59,7 +36,7 @@ const uploadImages = async (files) => {
       <Editor
         value={value}
         locale={zh_Hans}
-        plugins={plugins}
+        plugins={bytemdPlugins}
         onChange={(v) => {
           setValue(v)
         }}
