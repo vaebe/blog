9c3c9399e7c38ef59e28d972729e72652c12050e
vaebe <18137693952@163.com>
2024-08-25 23:20:56 +0800

    feat: github 接口使用 api 进行代理返回

---
+++ app/api/proxy/github/userInfo/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+
+export async function Get() {
+  const url = `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USER_NAME}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`获取 GitHub 用户信息失败: ${response.statusText}`);
+    }
+
+    const userInfo = await response.json();
+    return NextResponse.json(userInfo, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
