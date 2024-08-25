a195ada815dc23986bacede15c72d4a2fea81fd7
vaebe <18137693952@163.com>
2024-08-25 18:44:39 +0800

    feat: 获取 github 用户信息展示

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,8 +1,8 @@
-import { fetchGithubPinnedRepos } from './fetchGithubRepos'
-import type { GithubPinnedRepoInfo } from './fetchGithubRepos'
+import { fetchGithubPinnedRepos, fetchGithubUserInfo } from './fetchGithubInfo'
+import type { GithubPinnedRepoInfo, GithubUserInfo } from './fetchGithubInfo'
 import { fetchJuejinArticles } from './fetchJuejinArticles'
 import type { JuejinArticle, JuejinArticlesInfo } from './fetchJuejinArticles'
 
-export { fetchGithubPinnedRepos, fetchJuejinArticles }
+export { fetchGithubPinnedRepos, fetchGithubUserInfo, fetchJuejinArticles }
 
-export type { JuejinArticle, JuejinArticlesInfo, GithubPinnedRepoInfo }
+export type { JuejinArticle, JuejinArticlesInfo, GithubPinnedRepoInfo, GithubUserInfo }
