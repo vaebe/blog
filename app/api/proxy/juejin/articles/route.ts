6169479a4d2fd25de329cb2cbba6ee961b3a5675
vaebe <18137693952@163.com>
2024-08-26 21:34:11 +0800

    feat: 同步掘金文章信息

--- app/api/proxy/juejin/articles/route.ts
+++ app/api/proxy/juejin/articles/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server';
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
   try {
+      // 从 URL 获取查询参数
+      const { searchParams } = new URL(req.url);
+      const cursor = parseInt(searchParams.get('cursor') || '0');
+
+      console.log('12121', cursor)
+
     const res = await fetch('https://api.juejin.cn/content_api/v1/article/query_list', {
       method: 'POST',
       headers: {
@@ -10,13 +16,12 @@ export async function POST(req: Request) {
       body: JSON.stringify({
         "user_id": process.env.JUEJIN_USER_ID,
         "sort_type": 2,
-        "cursor": "0"
+        cursor: `${cursor}`
       })
     })
 
     const data = await res.json();
     return NextResponse.json(data, { status: 200 });
-
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
