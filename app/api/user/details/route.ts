b8647c7bf486981663bb5a27da1e921d37f3440d
vaebe <18137693952@163.com>
2024-08-27 22:35:02 +0800

    feat: github、用户名密码登录逻辑完善

---
+++ app/api/user/details/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import { sendJson } from '@/lib/utils'
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request) {
+  console.log(req, '-=-reqdsdasdsadasdsa=-=-')
+
+  const url = new URL(req.url);
+  const account = url.searchParams.get('account');
+
+  try {
+    if (!account) {
+      return sendJson({ code: -1, msg: '账号不存在' })
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { account: account },
+    });
+
+    return sendJson({ data: user })
+  } catch (error) {
+    console.log('校验用户失败', error)
+    return sendJson({ code: -1, msg: `校验用户失败` })
+  }
+}
