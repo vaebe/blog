c8fab826a66bb8ccdc4fe8667510ef7404595fbc
vaebe <18137693952@163.com>
2024-09-04 20:07:32 +0800

    feat: 增加路由组

---
+++ app/(base)/layout.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import LayoutHeader from '@/components/layout/header'
+import LayoutFooter from '@/components/layout/footer'
+
+export default function BaseLayout({ children, }: { children: React.ReactNode }) {
+  return (
+    <>
+      <LayoutHeader></LayoutHeader>
+      <main className="flex-grow">
+        {children}
+      </main>
+      <LayoutFooter></LayoutFooter>
+    </>
+  )
+}
