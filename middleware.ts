49be8d4938ae2c5458a491a64b8b501696ae6b8c
vaebe <18137693952@163.com>
2024-09-04 20:49:48 +0800

    feat(article): 文章新增页面需要授权才可以进入

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,26 @@
 import { withAuth } from "next-auth/middleware"
-const ADMIN_PATHS = ['/api/articles/delete', '/api/articles/add', '/api/articles/update', '/api/user'];
+
+const ADMIN_PAGES= ['/article/add']
+const ADMIN_APIS = ['/api/articles/delete', '/api/articles/add', '/api/articles/update', '/api/user'];
+const ADMIN_PATH_ALL = [...ADMIN_PAGES, ...ADMIN_APIS]
 
 export default withAuth(
   function middleware(req) {
     console.log("request:", req.method, req.url);
+
+    if (req.url.endsWith('')) {
+      console.log('文章路由')
+    }
   },
   {
     callbacks: {
       authorized: function ({ token , req}) {
         const pathname = req.nextUrl.pathname
         
-        if (ADMIN_PATHS.some(item => pathname.startsWith(item))) {
+        if (ADMIN_PATH_ALL.some(item => pathname.startsWith(item))) {
           return token?.role === '00'
         }
+        
         return true
       },
     },
@@ -20,5 +28,5 @@ export default withAuth(
 )
 
 export const config = {
-  matcher: ['/api/:path*'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
