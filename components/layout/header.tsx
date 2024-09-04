6903bbcbdd41dfc3b733a97d35a032065b6da2d6
vaebe <18137693952@163.com>
2024-09-04 21:16:16 +0800

    feat: 登录后增加头像展示

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,14 +6,15 @@ import { signOut, useSession } from "next-auth/react";
 import { routerList } from '@/lib/routers'
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 function NavList() {
   return (
     <ul className="flex space-x-1">
       {routerList.map(item => (
         <li key={item.path}>
-          <Link 
-            href={item.path} 
+          <Link
+            href={item.path}
             className="px-4 py-2 rounded-full text-gray-700 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-700 transition-all duration-300 ease-in-out"
           >
             {item.name}
@@ -26,7 +27,7 @@ function NavList() {
 
 export default function LayoutHeader() {
   const { theme, setTheme } = useTheme()
-  const { status } = useSession();
+  const { data: session, status } = useSession();
   const [scrolled, setScrolled] = useState(false)
 
   useEffect(() => {
@@ -53,17 +54,27 @@ export default function LayoutHeader() {
           </nav>
 
           <div className="flex items-center space-x-4">
-            <button 
-              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} 
+          {
+              status === 'authenticated' && (
+                <Avatar>
+                  <AvatarImage src={session.user.image as string} alt="@shadcn" />
+                  <AvatarFallback>{session.user.name ?? 'LL'}</AvatarFallback>
+                </Avatar>
+              )
+            }
+
+            <button
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
               className="p-2 rounded-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 ease-in-out transform hover:scale-110"
               aria-label={theme === 'dark' ? "Switch to light mode" : "Switch to dark mode"}
             >
               <Icon icon={theme === 'dark' ? "ph:sun-bold" : "ph:moon-bold"} className="w-5 h-5 text-gray-600 dark:text-gray-300" />
             </button>
 
+
             {status === 'authenticated' && (
-              <button 
-                onClick={() => signOut()} 
+              <button
+                onClick={() => signOut()}
                 className="p-2 rounded-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 ease-in-out transform hover:scale-110"
                 aria-label="Sign out"
               >
