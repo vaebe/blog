364afaa419560b699c5d2bd4c5085f0c1ebb30c0
vae <18137693952@163.com>
2024-09-14 10:12:39 +0800

    feat(api): 增加获取解析掘金文章详情的接口

---
+++ app/api/proxy/juejin/details/route.ts
@@ -0,0 +1,23 @@
+import { sendJson } from '@/lib/utils'
+import * as cheerio from 'cheerio'
+
+export async function GET(req: Request) {
+  try {
+    const url = new URL(req.url)
+    const id = url.searchParams.get('id') // 从查询参数中获取 id
+
+    const res = await fetch(`https://juejin.cn/post/${id}`, {
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8'
+      }
+    })
+
+    const htmlContent = await res.text()
+
+    const $ = cheerio.load(htmlContent)
+
+    return sendJson({ data: $('#article-root').html() })
+  } catch (error) {
+    return sendJson({ code: -1, msg: `${error}` })
+  }
+}
