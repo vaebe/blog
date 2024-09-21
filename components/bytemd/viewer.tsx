e87c9b7f5ea917563339224b0bfbe0b55fb6004c
vaebe <18137693952@163.com>
2024-09-21 21:55:43 +0800

    feat(bytemd): 完善编辑器深色主题样式并优化代码高亮

--- components/bytemd/viewer.tsx
+++ components/bytemd/viewer.tsx
@@ -3,6 +3,7 @@
 import { Viewer } from '@bytemd/react'
 import plugins from '@/components/bytemd/plugins'
 import './editor.scss'
+import './dark-theme.scss'
 
 export function BytemdViewer({ content }: { content: string }) {
   return <Viewer value={content} plugins={plugins}></Viewer>
