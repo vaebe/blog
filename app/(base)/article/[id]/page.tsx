cfb7c0f775a9e4cacf22161cc24da2bebfda3415
vaebe <18137693952@163.com>
2024-09-24 22:13:00 +0800

    style(article): 优化掘金文章详情页展示和阅读时间显示

--- app/(base)/article/[id]/page.tsx
+++ app/(base)/article/[id]/page.tsx
@@ -7,61 +7,80 @@ import { toast } from '@/components/hooks/use-toast'
 import { getReadingTime } from '@/lib/getReadingTime'
 import { Anchor } from './anchor/index'
 import { BytemdViewer } from '@/components/bytemd/viewer'
+import { Icon } from '@iconify/react'
 
 export default function Component({ params }: { params: { id: string } }) {
   const [pageType, setPageType] = useState<string>()
   const [article, setArticle] = useState<Article>()
   const [readingTime, setReadingTime] = useState<number>(0)
-
   const [details, setDetails] = useState<string>('')
 
   useEffect(() => {
-    async function getData() {
+    async function fetchArticleDetails() {
       const res = await fetch(`/api/articles/details?id=${params.id}`).then((res) => res.json())
       if (res.code !== 0) {
         toast({ variant: 'destructive', title: '警告', description: '获取文章详情失败!' })
-        return
+        return null
       }
-
-      setArticle(res.data)
-      setReadingTime(getReadingTime(res.data.content).minutes)
-      setPageType(res.data.source)
-
-      // 博客创建的文章直接返回
-      if (res.data.source === '00') return
-
-      fetch(`/api/proxy/juejin/details?id=${params.id}`)
-        .then((res) => res.json())
-        .then((res) => {
-          if (res.code !== 0) {
-            toast({ variant: 'destructive', title: '警告', description: '获取文章详情失败!' })
-            return
-          }
-
-          setDetails(res.data)
-        })
+      return res.data
     }
+
+    async function fetchProxyDetails() {
+      const res = await fetch(`/api/proxy/juejin/details?id=${params.id}`).then((res) => res.json())
+      if (res.code !== 0) {
+        toast({ variant: 'destructive', title: '警告', description: '获取代理文章详情失败!' })
+        return null
+      }
+      return res.data
+    }
+
+    async function getData() {
+      const articleData = await fetchArticleDetails()
+      if (!articleData) return
+
+      setArticle(articleData)
+      setReadingTime(getReadingTime(articleData.content).minutes)
+      setPageType(articleData.source)
+
+      if (articleData.source !== '00') {
+        const proxyDetails = await fetchProxyDetails()
+        if (proxyDetails) {
+          setDetails(proxyDetails)
+        }
+      }
+    }
+
     getData()
   }, [params.id])
 
   return (
-    <div className="flex justify-center items-start">
-      <Card className="w-full max-w-4xl my-10 p-4 sm:px-6 lg:px-8">
-        <CardHeader className="px-0">
+    <div className="flex justify-center items-start gap-4 mt-[4vh]">
+      <Card className="w-full max-w-4xl">
+        <CardHeader className="space-y-2 border-b px-4 sm:px-6 lg:px-8">
           <CardTitle>{article?.title}</CardTitle>
-          <CardDescription>阅读 {readingTime} 分钟</CardDescription>
-          <CardDescription>{article?.summary}</CardDescription>
+          <CardDescription className="text-sm text-gray-400 flex items-center">
+            <Icon icon="ri:time-line" />
+            阅读时间: {readingTime} 分钟
+          </CardDescription>
         </CardHeader>
 
-        {pageType === '00' ? (
-          <BytemdViewer content={article?.content ?? ''} />
-        ) : (
-          <div dangerouslySetInnerHTML={{ __html: details }} />
-        )}
+        <CardContent className="mt-4 px-4 sm:px-6 lg:px-8">
+          <div>
+            <h2 className="text-xl font-semibold mb-2">总结:</h2>
+            <p className="text-gray-600 bg-gray-100 p-4 rounded-md">{article?.summary}</p>
+          </div>
+
+          {pageType === '00' ? (
+            <BytemdViewer content={article?.content ?? ''} />
+          ) : (
+            <div dangerouslySetInnerHTML={{ __html: details }} />
+          )}
+        </CardContent>
       </Card>
 
-      <Card className="sticky top-[12vh] ml-4 w-[240px] hidden lg:block">
-        <CardContent className="pt-4">
+      <Card className="sticky top-14 w-64 hidden lg:block h-fit max-h-[calc(100vh-4rem)] overflow-y-auto">
+        <CardContent className="p-4">
+          <h3 className="text-lg font-semibold mb-4">目录</h3>
           <Anchor content={details || article?.content || ''}></Anchor>
         </CardContent>
       </Card>
