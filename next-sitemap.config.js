ac8d08da4b99e8503b7b8ead7ac5113bff16413d
vaebe <18137693952@163.com>
2024-09-29 15:37:46 +0800

    chore: 打包时为文章生成站点地图

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,28 @@
 /** @type {import('next-sitemap').IConfig} */
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 module.exports = {
   siteUrl: process.env.SITE_URL,
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  exclude: ['/api', '/api/**'],
+  additionalPaths: async () => {
+    const articles = await prisma.article.findMany({
+      orderBy: { createdAt: 'desc' }
+    })
+
+    const result = []
+
+    articles.map((item) => {
+      result.push({
+        loc: `${process.env.SITE_URL}/article/${item.id}`, // 页面位置
+        changefreq: 'daily', // 更新频率
+        priority: 0.7, // 优先级
+        lastmod: new Date(item.createdAt).toISOString() // 最后修改时间
+      })
+    })
+
+    return result
+  }
 }
