fb7d8f3cf57f7d7549973e9c5313a85fdfff43e5
vae <18137693952@163.com>
2024-10-11 17:42:21 +0800

    feat(guestbook): 拆分留言板页面组件并优化功能

---
+++ app/(base)/guestbook/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <div className="my-6">
+      <h2 className="text-4xl font-bold">欢迎来到我的留言板</h2>
+
+      <p className="w-4/12 mt-6 text-gray-500">
+        在这里你可以留下一些内容、对我说的话、建议、你的想法等等一切不违反中国法律的内容!
+        <br />
+        然后就是你不能骂我,不然我会删🤔!
+      </p>
+    </div>
+  )
+}
