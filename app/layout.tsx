17ed095b0e1f0ae668a87afc8b72d4e674c99636
vae <18137693952@163.com>
2024-10-18 15:35:04 +0800

    feat: 增加背景组件

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from 'next-themes'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeSwitch } from '@/components/theme-switch'
+import { Artdots } from '@/components/artdots'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <SessionProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Artdots className="fixed top-0 left-0 z-[-1]"></Artdots>
             <ThemeSwitch />
           </ThemeProvider>
         </SessionProvider>
