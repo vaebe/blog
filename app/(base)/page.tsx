2ac62e0355649c1b524a3047b95c8daf087a16e6
vaebe <18137693952@163.com>
2024-10-22 21:10:49 +0800

    style: 内容最大宽度

--- app/(base)/page.tsx
+++ app/(base)/page.tsx
@@ -5,7 +5,7 @@ import { ArchiveInfo } from '@/app/(base)/home/archiveInfo'
 
 export default function Component() {
   return (
-    <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <LatestArticles></LatestArticles>
       <ArchiveInfo></ArchiveInfo>
     </main>
