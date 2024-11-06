3781ff0cdb7c5c247aa61f406f6d69080063c47d
vaebe <18137693952@163.com>
2024-11-06 21:08:11 +0800

    feat(api): 为掘金和github用户信息接口添加缓存机制

--- lib/enums.ts
+++ lib/enums.ts
@@ -10,3 +10,10 @@ export const techIcons: Record<string, string> = {
 }
 
 export const techStackData = ['TypeScript', 'Vue', 'NuxtJs', 'NextJs', 'NestJs', 'Go', 'Mysql']
+
+export const TimeInSeconds = {
+  oneHour: 3600, // 1小时 = 3600秒
+  oneDay: 86400, // 1天 = 86400秒
+  oneWeek: 604800, // 1周 = 604800秒
+  oneMonth: 2592000 // 1个月 = 2592000秒（以30天计算）
+}
