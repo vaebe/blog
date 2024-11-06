d08ba6c4a81d4399260734f14152f7d8f26af7a5
vaebe <18137693952@163.com>
2024-11-06 21:08:11 +0800

    feat(api): 获取掘金、github 用户信息接口增加缓存

--- app/api/proxy/github/userInfo/route.ts
+++ app/api/proxy/github/userInfo/route.ts
@@ -1,23 +1,40 @@
 import { sendJson } from '@/lib/utils'
+import { kv } from '@vercel/kv'
+import type { GithubUserInfo } from '@/types/github'
+import { TimeInSeconds } from '@/lib/enums'
+
+const InfoKey = 'blog-github-user-info'
+const GITHUB_API_URL = `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USER_NAME}`
+
+async function fetchUserInfo() {
+  const response = await fetch(GITHUB_API_URL, {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`
+    }
+  })
+
+  if (!response.ok) {
+    const errorMessage = await response.text()
+    throw new Error(`获取 GitHub 用户信息失败: ${response.statusText} - ${errorMessage}`)
+  }
+
+  return response.json()
+}
 
 export async function GET() {
-  const url = `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USER_NAME}`
-
   try {
-    const response = await fetch(url, {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`
-      }
-    })
-
-    if (!response.ok) {
-      return sendJson({ code: -1, msg: `获取 GitHub 用户信息失败: ${response.statusText}` })
+    const cachedInfo = await kv.get<GithubUserInfo>(InfoKey)
+    if (cachedInfo) {
+      return sendJson({ data: cachedInfo })
     }
 
-    const userInfo = await response.json()
+    const userInfo = await fetchUserInfo()
+    await kv.set(InfoKey, userInfo, { ex: TimeInSeconds.oneWeek })
+
     return sendJson({ data: userInfo })
   } catch (error) {
-    return sendJson({ code: -1, msg: `获取 GitHub 用户信息失败: ${error}` })
+    console.error(error)
+    return sendJson({ code: -1, msg: '获取 GitHub 用户信息失败!' })
   }
 }
--- app/api/proxy/juejin/userInfo/route.ts
+++ app/api/proxy/juejin/userInfo/route.ts
@@ -1,24 +1,43 @@
 import { sendJson } from '@/lib/utils'
+import { kv } from '@vercel/kv'
+import type { JuejinUserInfo } from '@/types/juejin'
+import { TimeInSeconds } from '@/lib/enums'
+
+const InfoKey = 'blog-juejin-user-info'
+const JUEJIN_API_URL = `https://api.juejin.cn/user_api/v1/user/get?user_id=${process.env.JUEJIN_USER_ID}`
+
+// 获取掘金用户信息
+async function fetchUserInfo() {
+  const response = await fetch(JUEJIN_API_URL)
+
+  if (!response.ok) {
+    const errorInfo = await response.json()
+    throw new Error(errorInfo.msg || 'Failed to fetch user info from Juejin')
+  }
+  const info = await response.json()
+
+  if (info.err_no !== 0) {
+    throw new Error(info.err_msg)
+  }
+  return info.data
+}
 
 export async function GET() {
   try {
-    const res = await fetch(
-      `https://api.juejin.cn/user_api/v1/user/get?user_id=${process.env.JUEJIN_USER_ID}`
-    )
-
-    if (!res.ok) {
-      const info = await res.json()
-      return sendJson({ code: -1, msg: info })
+    // 取出缓存
+    const cachedInfo = await kv.get<JuejinUserInfo>(InfoKey)
+    if (cachedInfo) {
+      return sendJson({ data: cachedInfo })
     }
 
-    const info = await res.json()
+    const userInfo = await fetchUserInfo()
 
-    if (info.err_no !== 0) {
-      return sendJson({ code: -1, msg: info.err_msg })
-    }
+    // 缓存数据
+    await kv.set(InfoKey, userInfo, { ex: TimeInSeconds.oneDay })
 
-    return sendJson({ data: info.data })
+    return sendJson({ data: userInfo })
   } catch (error) {
+    console.error(error)
     return sendJson({ code: -1, msg: '获取掘金用户信息失败!' })
   }
 }
