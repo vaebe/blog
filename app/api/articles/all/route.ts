6cda08429856164f59dd999879340b6babdc957b
vaebe <18137693952@163.com>
2024-11-06 21:19:46 +0800

    feat(api): 为获取全部文章接口添加缓存机制

--- app/api/articles/all/route.ts
+++ app/api/articles/all/route.ts
@@ -1,11 +1,25 @@
 import { sendJson } from '@/lib/utils'
 import { prisma } from '@/prisma'
+import { Article } from '@prisma/client'
+import { kv } from '@vercel/kv'
+import { TimeInSeconds } from '@/lib/enums'
+
+const InfoKey = 'blog-all-articles'
 
 export async function GET() {
+  const cachedInfo = await kv.get<Article[]>(InfoKey)
+
+  if (cachedInfo) {
+    return sendJson({ data: cachedInfo })
+  }
+
   try {
     const articles = await prisma.article.findMany({
       orderBy: { createdAt: 'desc' }
     })
+
+    await kv.set(InfoKey, articles, { ex: TimeInSeconds.oneDay })
+
     return sendJson({ data: articles })
   } catch (error) {
     return sendJson({ code: -1, msg: `获取所有文章失败: ${error}` })
