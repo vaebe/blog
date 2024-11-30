56e17fdba665c8f964f8b11ce164166760113cda
vaebe <18137693952@163.com>
2024-11-30 18:51:45 +0800

    feat(layout): 改成服务端渲染

---
+++ app/providers.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
+import { Toaster } from '@/components/ui/toaster'
+import { Toaster as SonnerToaster } from '@/components/ui/sonner'
+import { Artdots } from '@/components/artdots'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+        <Artdots className="fixed top-0 left-0 z-[-1]" />
+        <Toaster />
+        <SonnerToaster position="bottom-center" />
+      </ThemeProvider>
+    </SessionProvider>
+  )
+}
