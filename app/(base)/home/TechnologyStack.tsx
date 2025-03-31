7f86a1f5b56d264f91100e4da9dd72bbad513f1d
vaebe <18137693952@163.com>
2025-03-31 19:34:19 +0800

    style(home): 用户信息骨架屏优化

--- app/(base)/home/TechnologyStack.tsx
+++ app/(base)/home/TechnologyStack.tsx
@@ -5,7 +5,7 @@ import { ContentCard } from './ContentCard'
 export function TechnologyStack() {
   return (
     <ContentCard title="技术栈">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {techStackData.map((tech) => (
           <div
             key={tech}
