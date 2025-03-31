a15533d486fb1c09cec4ccbe3d07c7eff50fc411
vaebe <18137693952@163.com>
2025-03-31 18:40:29 +0800

    feat(api): 移除 github 和掘金用户信息接口的 kv 缓存并添加 revalidate 机制

--- app/api/proxy/github/userInfo/route.ts
+++ app/api/proxy/github/userInfo/route.ts
@@ -1,9 +1,5 @@
 import { sendJson } from '@/lib/utils'
-import { kv } from '@vercel/kv'
-import type { GithubUserInfo } from '@/types/github'
-import { TimeInSeconds } from '@/lib/enums'
 
-const InfoKey = 'blog-github-user-info'
 const GITHUB_API_URL = `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USER_NAME}`
 
 async function fetchUserInfo() {
@@ -24,14 +20,7 @@ async function fetchUserInfo() {
 
 export async function GET() {
   try {
-    const cachedInfo = await kv.get<GithubUserInfo>(InfoKey)
-    if (cachedInfo) {
-      return sendJson({ data: cachedInfo })
-    }
-
     const userInfo = await fetchUserInfo()
-    await kv.set(InfoKey, userInfo, { ex: TimeInSeconds.oneWeek })
-
     return sendJson({ data: userInfo })
   } catch (error) {
     console.error(error)
