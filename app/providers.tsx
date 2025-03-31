e3090e7230c927dbd81b46d53dd6c6e0045b23fa
vae <18137693952@163.com>
2025-03-31 10:42:16 +0800

    feat: 使用 simple-git-hooks 替换 husky

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
-        <Toaster position="bottom-center" />
+        <Toaster position="bottom-right" />
       </ThemeProvider>
     </SessionProvider>
   )
