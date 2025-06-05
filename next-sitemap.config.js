896ffce95ed4efe11e5046366ce7b8f9bcefce3f
vae <18137693952@163.com>
2025-06-05 15:12:04 +0800

    chore: 生成 sitemap 文章数据从接口获取

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next-sitemap').IConfig} */
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
 
-const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
+const { NEXT_PUBLIC_SITE_URL } = process.env
 
 module.exports = {
   siteUrl: NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
-  exclude: ['/api', '/api/**'],
+  exclude: ['/api', '/api/**', '/article/add'],
   additionalPaths: async () => {
-    const articles = await prisma.article.findMany({
-      orderBy: { createdAt: 'desc' }
-    })
+    let articles = []
+
+    // 本地打包不会生成-因为调用接口会失败！
+    try {
+      const res = await fetch(`${NEXT_PUBLIC_SITE_URL}/api/articles/all`)
+      const json = await res.json()
+      articles = json.code === 0 ? json.data : []
+    } catch {
+      articles = []
+    }
 
     const result = []
 
@@ -20,7 +25,7 @@ module.exports = {
       result.push({
         loc: `${NEXT_PUBLIC_SITE_URL}/article/${item.id}`, // 页面位置
         changefreq: 'daily', // 更新频率
-        priority: 0.7, // 优先级
+        priority: 0.8, // 优先级
         lastmod: new Date(item.createdAt).toISOString() // 最后修改时间
       })
     })
