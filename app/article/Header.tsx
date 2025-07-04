09c32177136d7e6e84548ae2e8cf8ebe4afbe265
vae <18137693952@163.com>
2025-07-04 14:13:36 +0800

    feat(article): 文章发布编辑

--- app/article/Header.tsx
+++ app/article/Header.tsx
@@ -1,32 +1,99 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { PublishArticleInfo } from '@/types'
-import type { Dispatch, SetStateAction } from 'react'
+import { AnyObject, PublishArticleInfo } from '@/types'
+import { PublishDialog } from '@/app/article/PublishDialog'
+import { useRouter, usePathname } from 'next/navigation'
+import { toast } from 'sonner'
 import { Updater } from 'use-immer'
 
-interface HeaderProps {
-  updateArticleInfo: Updater<PublishArticleInfo>
-  setPublishDialogShow: Dispatch<SetStateAction<boolean>>
-  title: string
-  butName: string
+const SubmitArticleConfig = {
+  add: {
+    url: '/api/articles/add',
+    method: 'POST',
+    successMsg: '文章发布成功!',
+    errorMsg: '发布文章时出现错误!'
+  },
+  update: {
+    url: '/api/articles/update',
+    method: 'PUT',
+    successMsg: '编辑成功!',
+    errorMsg: '编辑文章时出现错误!'
+  }
 }
 
-function HeaderComponent({ updateArticleInfo, setPublishDialogShow, title, butName }: HeaderProps) {
+async function submitArticle(info: PublishArticleInfo, type: 'add' | 'update') {
+  if (!info.title) {
+    toast('文章标题不能为空!')
+    return
+  }
+
+  if (!info.content) {
+    toast('文章内容不能为空!')
+    return
+  }
+
+  const config = SubmitArticleConfig[type]
+
+  try {
+    const res = await fetch(config.url, {
+      method: config.method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(info)
+    }).then((res) => res.json())
+
+    if (res.code === 0) {
+      toast(config.successMsg)
+      return 'success'
+    } else {
+      toast(config.errorMsg)
+    }
+  } catch (error) {
+    console.error(error)
+    toast(config.errorMsg)
+  }
+}
+
+interface HeaderProps {
+  articleInfo: PublishArticleInfo
+  publishButName: string
+  updateArticleInfo: Updater<PublishArticleInfo>
+}
+
+function HeaderComponent({ publishButName, articleInfo, updateArticleInfo }: HeaderProps) {
+  const pathName = usePathname()
+
+  const router = useRouter()
+
+  function onPublish(info: AnyObject) {
+    const api = pathName.includes('edit')
+      ? submitArticle({ ...articleInfo, ...info }, 'update')
+      : submitArticle({ ...articleInfo, ...info }, 'add')
+
+    api.then((res) => {
+      if (res === 'success') {
+        router.push('/article/list')
+      }
+    })
+  }
+
   return (
     <div className="flex items-center justify-between border-b border-b-gray-200">
       <Input
-        value={title}
+        value={articleInfo.title}
         onChange={(e) =>
           updateArticleInfo((d) => {
             d.title = e.target.value
           })
         }
         placeholder="输入文章标题..."
-        className="bg-white text-xl text-black font-bold border-none rounded-none shadow-none ring-0 !ring-offset-0 focus-visible:ring-0"
+        className="bg-white !text-2xl text-black font-medium border-none rounded-none shadow-none ring-0 !ring-offset-0 focus-visible:ring-0"
       />
-      <Button className="rounded-none shadow-none" onClick={() => setPublishDialogShow(true)}>
-        {butName}
-      </Button>
+
+      <PublishDialog articleInfo={articleInfo} onPublish={onPublish}>
+        <Button className="rounded-none shadow-none cursor-pointer">{publishButName}</Button>
+      </PublishDialog>
     </div>
   )
 }
