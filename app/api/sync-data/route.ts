ba4332522b540415d65b1bc7a3a5e6c5ef5c1d23
vaebe <18137693952@163.com>
2025-09-07 22:07:23 +0800

    feat(sync-data): 添加同步数据的详细信息输出

--- app/api/sync-data/route.ts
+++ app/api/sync-data/route.ts
@@ -2,7 +2,6 @@ import { sendJson } from '@/lib/utils'
 import { getArticles } from './juejin-data'
 import { saveGitHubPinnedReposToCache } from '@/lib/github/pinned-repos'
 import { saveGithubUserInfoToCache } from '@/lib/github/user-info'
-
 export async function GET(req: Request) {
   const apiKey = req.headers.get('x-api-key')
   const expectedApiKey = process.env.GITHUB_REPOSITORY_API_KEY
@@ -25,7 +24,11 @@ export async function GET(req: Request) {
     const syncArticleNameList = await getArticles(0)
     console.log(syncArticleNameList, '/r/n')
 
-    return sendJson({ code: 0, msg: '同步数据成功' })
+    return sendJson({
+      code: 0,
+      msg: '同步数据成功',
+      data: { syncArticleNameList, gitHubPinnedReposToCacheRes, githubUserInfoToCacheRes }
+    })
   } catch (error) {
     return sendJson({ code: -1, msg: `同步数据失败：${error}` })
   }
