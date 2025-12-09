fd87ba7597d66c250f9cc0527ac0db060ca8cf08
vae <18137693952@163.com>
2025-12-09 10:14:09 +0800

    fix(prisma): new PrismaMariaDb 配置错误导致数据库无法连接

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,8 +7,13 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  connectionLimit: 5
+  port: parseInt(process.env.DATABASE_PORT || '3306'),
+  connectionLimit: 5,
 });
-const prisma = new PrismaClient({ adapter });
+
+const prisma = new PrismaClient({ 
+  adapter,
+  log: ['error', 'warn'],
+});
 
 export { prisma }
