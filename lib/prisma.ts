f81340dbfe581a77e8faac45b666025b7ad4835b
vae <18137693952@163.com>
2026-01-14 16:17:15 +0800

    feat: prisma 增加单例模式

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,9 +11,21 @@ const adapter = new PrismaMariaDb({
   connectionLimit: 10
 })
 
-const prisma = new PrismaClient({
-  adapter,
-  log: ['error', 'warn']
-})
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter,
+    log: ['error', 'warn']
+  })
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export { prisma }
